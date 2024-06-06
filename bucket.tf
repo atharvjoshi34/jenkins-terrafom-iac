192687d13ca5eb69fd47e35f559aee2dded2a246
atharv <atharvjoshi34@gmail.com>
2024-06-06 14:21:42 +0530

    adding log bucket

--- bucket.tf
+++ bucket.tf
@@ -4,3 +4,10 @@ resource "google_storage_bucket" "backend-bucket" {
     location = "US" 
   
 }
+
+resource "google_storage_bucket" "log-bucket" {
+    name = "peerless-haiku-423005-k9-log"
+    location = "US" 
+  
+}
+
